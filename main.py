d3de0f8b77caff88752c949051490b3e7e4c5173
Angels1234Lens <164226888+Angels1234Lens@users.noreply.github.com>
2024-03-30 14:49:52 -0500

    hola este es mi primer commir

---
+++ main.py
@@ -0,0 +1,1 @@
+print("esta es la historia de renny\n Renny es un joven de 18 años que le gusta el ajedrez\nSu sueño actualmente es un poco mas realista, antes queria ser medallista olimpico de taekwondo, no lo logro y ahora solo quiere vivir de forma estable, y con un buen sueldo\n ¿Un poco mas realista no?" )
